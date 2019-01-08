c188cc7fc78cf497334165cb723e365e9346ddb1
Ayush Yadav <46446665+yadavayush0808@users.noreply.github.com>
2019-01-08 15:30:44 +0530

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
 cout<<"hello world";
-cout<<"hey this is a master branch code";
+cout<<"hey this is a child branch code";
 
 return 0;
 }
 
 //thank you
 
-//hey this is next branch code
+//hey this is child branch code
